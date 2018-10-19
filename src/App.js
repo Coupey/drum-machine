40645f3f1b05eac676892aedf040a07187f7ec37
Paul Coupe <paul@coupey.co.uk>
2018-10-19 16:18:30 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -7,18 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div class="beatgrid">
+            <div className="beat inline current-beat"></div>
+            <div className="beat inline"></div>
+            <div className="beat inline selected-beat"></div>
+            <div className="beat inline"></div>
+            <div className="beat inline"></div>
+            <div className="beat inline"></div>
+            <div className="beat inline"></div>
+            <div className="beat inline"></div>
+          </div>
         </header>
       </div>
     );
