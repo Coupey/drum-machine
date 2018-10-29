b4054e4984801712500d0bc6fe96fded4c46d670
Paul Coupe <paul@coupey.co.uk>
2018-10-29 10:01:22 +0000

    Updates

---
+++ precache-manifest.21e2ec77e167a736046291b93ff4edbb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c0c783819ccf789883a56546e0c1e4",
+    "url": "/drum-machine-react/static/media/snare-808.24c0c783.mp3"
+  },
+  {
+    "revision": "dce87dcc55d77c0658dc4304671ac5f4",
+    "url": "/drum-machine-react/static/media/kick-electro01.dce87dcc.mp3"
+  },
+  {
+    "revision": "9da5978c0c6962778526dcb8f15b3380",
+    "url": "/drum-machine-react/static/media/hihat-808.9da5978c.mp3"
+  },
+  {
+    "revision": "789d5cf7884c7f72ec07",
+    "url": "/drum-machine-react/static/js/runtime~main.789d5cf7.js"
+  },
+  {
+    "revision": "ebe4a2cbd17d11d42ca0",
+    "url": "/drum-machine-react/static/js/main.ebe4a2cb.chunk.js"
+  },
+  {
+    "revision": "f49d05b2ab6058d169d4",
+    "url": "/drum-machine-react/static/js/1.f49d05b2.chunk.js"
+  },
+  {
+    "revision": "ebe4a2cbd17d11d42ca0",
+    "url": "/drum-machine-react/static/css/main.09bb9336.chunk.css"
+  },
+  {
+    "revision": "550b244b5400c2000476cadbf01bf42d",
+    "url": "/drum-machine-react/index.html"
+  }
+];
